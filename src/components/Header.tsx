f0bb2de74f63c5d88c34acba5f8ca343b1fc3b25
stopatinfinity <nevinchoi@gmail.com>
2021-05-02 19:42:28 -0400

    add routing for about page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
+import { useLocation } from 'react-router-dom'
 import Button from './button'
 
 const Header = (props: {title: any, onAdd: any, showAdd: any}) => {
+    const location = useLocation();
     return (
         <header className='header'>
             <h1>{props.title}</h1>
-            <Button color={props.showAdd ? 'red' : 'green'} text={props.showAdd ? 'Close' : 'Add'} 
-            onClick={props.onAdd} />
+            {location.pathname === '/' && <Button color={props.showAdd ? 'red' : 'green'} text={props.showAdd ? 'Close' : 'Add'}
+            onClick={props.onAdd} />}
         </header>
     )
 }
